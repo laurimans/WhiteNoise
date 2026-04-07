f1311fd8b29e1f733523a6deb60d07a2d0488682
laurimans <163913448+laurimans@users.noreply.github.com>
2026-04-07 15:50:44 +0200

    Manager y UI del Diario

---
+++ Maya/Assets/Scripts/Gameplay/JournalManager.cs
@@ -0,0 +1,60 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class JournalEntry
+{
+    [SerializeField] private string date;
+    [SerializeField] [TextArea] private string body;
+    [SerializeField] private List<string> clues;
+
+    public JournalEntry(string _date, string _body)
+    {
+        this.date = _date;
+        this.body = _body;
+        clues = new List<string>();
+    }
+
+    public string GetDate() => date;
+    public string GetBody() => body;
+    public string GetClue(int index) => clues[index];
+
+    public void AddClue(string clueToAdd)
+    {
+        clues.Add(clueToAdd);
+    }
+}
+
+public class JournalManager : MonoBehaviour
+{
+    [SerializeField] public List<JournalEntry> journalEntries;
+
+    public int GetEntriesCount() => journalEntries.Count;
+
+    public JournalEntry GetEntry(int index)
+    {
+        return journalEntries[index];
+    }
+
+    public JournalEntry GetLastEntry()
+    {
+        return journalEntries[journalEntries.Count - 1];
+    }
+
+
+    public void AddEntry(string _date, string _body)
+    {
+        JournalEntry newEntry = new JournalEntry(_date, _body);
+        journalEntries.Add(newEntry);
+    }
+
+    public void AddClue(int index, string clue)
+    {
+        journalEntries[index].AddClue(clue);
+    }
+
+
+}
+    
+
--- Maya/Assets/Scripts/UI/JournalUI.cs
+++ Maya/Assets/Scripts/UI/JournalUI.cs
@@ -1,8 +1,21 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JournalUI : MonoBehaviour
 {
     [SerializeField] private GameObject HUDPanel;
+    [SerializeField] private JournalManager journal;
+
+    [Header("Journal Texts")]
+    [SerializeField] private TextMeshProUGUI dateText;
+    [SerializeField] private TextMeshProUGUI bodyText;
+
+    [Header("Journal Buttons")]
+    [SerializeField] private Button nextButton;
+    [SerializeField] private Button prevButton;
+
+    private int currentIndex = 0;
 
     private void Start()
     {
@@ -11,7 +24,6 @@ public class JournalUI : MonoBehaviour
 
     private void OnEnable()
     {
-        this.gameObject.SetActive(true);
         HUDPanel.SetActive(false);
     }
 
@@ -29,16 +41,46 @@ public class JournalUI : MonoBehaviour
 
     public void NextPage()
     {
-
+        if (currentIndex < journal.GetEntriesCount() - 1)
+        {
+            currentIndex++;
+            UpdateJournalUI();
+        }
     }
 
     public void PreviousPage()
     {
-
+        if (currentIndex > 0)
+        {
+            currentIndex--;
+            UpdateJournalUI();
+        }
     }
 
     public void OpenJornal()
     {
+        currentIndex = journal.GetEntriesCount() - 1;
+
+        UpdateJournalUI();
+
         this.gameObject.SetActive(true);
     }
+
+    private void UpdateJournalUI()
+    {
+        JournalEntry entry = journal.GetEntry(currentIndex);
+
+        if (entry != null)
+        {
+            dateText.text = entry.GetDate();
+            bodyText.text = entry.GetBody();
+        }
+
+        // Bloqueo botones
+        int totalEntries = journal.GetEntriesCount();
+
+        nextButton.interactable = currentIndex < totalEntries - 1;
+        prevButton.interactable = currentIndex > 0;
+    }
+
 }
